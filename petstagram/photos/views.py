0ab4ca35934f1d8a62a15b002c2bff41fc1bf6b5
Tea <66689566+Velicanstvena@users.noreply.github.com>
2026-02-26 22:47:06 +0100

    Initial Commit

---
+++ petstagram/photos/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+
+from petstagram.common.forms import CommentForm
+from petstagram.photos.forms import PhotoCreateForm, PhotoEditForm
+from petstagram.photos.models import Photo
+
+
+def add_photo(request):
+    form = PhotoCreateForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    context = {'form': form}
+
+    return render(request, template_name='photos/photo-add-page.html', context=context)
+
+
+def show_photo_details(request, pk):
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+    comment_form = CommentForm()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+
+    return render(request, template_name='photos/photo-details-page.html', context=context)
+
+
+def edit_photo(request, pk):
+    photo = Photo.objects.filter(pk=pk).get()
+    if request.method == 'GET':
+        form = PhotoEditForm(instance=photo)
+    else:
+        form = PhotoEditForm(request.POST, instance=photo)
+        if form.is_valid():
+            form.save()
+            return redirect('photo-details', pk=pk)
+
+    context = {
+        'form': form,
+        'photo': photo,
+    }
+
+    return render(request, 'photos/photo-edit-page.html', context)
+
+
+def delete_photo(request, pk):
+    photo = Photo.objects.get(pk=pk)
+    photo.delete()
+    return redirect('home')
